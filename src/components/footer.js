5b05020a9faad068d8402194d59b91c0fecdee50
Carlo Bilbao <cbilbao88@gmail.com>
2018-04-14 13:17:50 -0700

    chore: update footer with more relevant information

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,49 +1,22 @@
 import React from "react";
-import footerImage from "../img/img-logo-footer.png"
+import footerImage from "../img/magnolia-flower.png"
 
 const obFooter = () => {
   return (
     <footer id="colophon" className="site-footer bg-black" role="contentinfo">
       <div className="container">
-        <div className="row">
-          <div className="col-md-4 col-sm-6">
+        <div className="row center-text">
+          <div className="col-md-6 col-sm-6">
             <section className="widget">
-              <img src={footerImage} alt="bootstrap theme for construction companies" className="img-responsive marg-20-btm marg-20-top"/>
-              <p>Construction company based on Orange, California. Construction was very sufficient in maintaining the integrity of project in terms of planning, scheduling, cost and quality.</p>
+              <h3 className="widget-title">Our Summary</h3>
+              <p>A residential care facility located in Concord, California. We believe in providing the safest, most reliable, and compassionate care home service that fits a client’s daily needs in a care facility.</p>
             </section>{/* .widget */}
           </div>{/* .col-## */}
 
-          <div className="col-md-2 col-sm-6">
-            <section className="widget">
-              <h3 className="widget-title">Our Company</h3>
-              <ul>
-                <li><a href="#">About us</a></li>
-                <li><a href="#">Service</a></li>
-                <li><a href="#">Selected Work</a></li>
-                <li><a href="#">Get in touch</a></li>
-                <li><a href="#">Careers</a></li>
-              </ul>
-            </section>{/* .widget */}
-          </div>{/* .col-## */}
-
-          <div className="col-md-3 col-sm-6">
-            <section className="widget">
-              <h3 className="widget-title">Our Services</h3>
-              <ul>
-                <li><a href="#">Metal Roofting</a></li>
-                <li><a href="#">Green Building</a></li>
-                <li><a href="#">Construction Consultant</a></li>
-                <li><a href="#">Construction Management</a></li>
-                <li><a href="#">General Contracting</a></li>
-              </ul>
-            </section>{/* .widget */}
-          </div>{/* .col-## */}
-
-          <div className="col-md-3 col-sm-6">
+          <div className="col-md-6 col-sm-6">
             <section className="widget">
               <h3 className="widget-title">Our Contact</h3>
-              <p>5252 E Walnut Ave, Orange, California, United States.</p>
-              <p>hello@construction.me<br/>+62 (0) 274 441 5005</p>
+              <p>Concord, California, United States. | magnoliaghllc@aol.com<br/>925-483-2610</p>
             </section>
           </div>{/* .col-## */}
         </div>{/* .row */}
